7f3c57932e3648bed461260e3a16912fd0ea2ce9
Diego Agudo <diegoagudo@pop-os.localdomain>
2022-10-25 18:28:53 -0300

    Prueba 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="row d-flex align-items-center justify-content-around">
         <img className='logo col-1' src={logo} alt="logo A3D" />
         <div className="col-sm-10 d-flex justify-content-around">
-          <Link className='link' to={'/'}><p className='link'>Home</p></Link>
+          <Link className='link' to={'/Portfolio_V2'}><p className='link'>Home</p></Link>
           <Link className='link' to={'/about'}><p className='link'>About</p></Link>
           <Link className='link' to={'/projects'}><p className='link'>Projects</p></Link>
           <Link className='link' to={'/contact'}><p className='link'>Contact</p></Link> 
