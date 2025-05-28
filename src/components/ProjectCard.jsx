415daf2d598c19c29a5df6a632c754af0564092b
piteca <piteca@pop-os.localdomain>
2025-05-28 11:51:16 -0300

    se agrega link a github

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -19,6 +19,13 @@ const CardBody = ({ project }) => {
       </div>
       <div className='cardInf d-flex justify-content-around align-items-center bg-dark p-1'>
         <h3 className='pText' style={{ color: `${project.color}` }}>{project.category}</h3>
+        <h3 className='pText' style={{ color: project.color }}>
+          {project.linkGithub !== "" && (
+          <a className='linkgit' href={project.linkGithub} target="_blank" rel="noopener noreferrer">
+            <img src={require('../images/logos/linkgithub.png')} alt="GitHub" style={{ height: '24px' }} />
+          </a>
+          )}
+        </h3>
         <h2 className='pNumber' style={{ color: `${project.color}` }}>{project.number}</h2>
       </div>
     </>
@@ -32,7 +39,7 @@ const ProjectCard = ({ project, handleCardClick }) => {
       (<a href={project.url} target="blank" className='pCard m-3 fill-1'>
         <CardBody project={project} />
       </a>) :
-      (<div className='pCard m-3 fill-1' onClick={()=> handleCardClick(project)}>
+      (<div className='pCard m-3 fill-1' onClick={() => handleCardClick(project)}>
         <CardBody project={project} />
       </div>)
   );
